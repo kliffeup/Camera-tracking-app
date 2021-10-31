ad8ab590f9a0986a75a5dec6a0d9983af9cf96b1
kliffeup <azatvaleev356778@gmail.com>
2021-10-31 23:18:20 +0300

    hw #2 done

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -6,6 +6,7 @@ __all__ = [
 
 from typing import List, Optional, Tuple
 
+import cv2
 import numpy as np
 
 from corners import CornerStorage
@@ -17,7 +18,11 @@ from _camtrack import (
     calc_point_cloud_colors,
     pose_to_view_mat3x4,
     to_opencv_camera_mat3x3,
-    view_mat3x4_to_pose
+    view_mat3x4_to_pose,
+    triangulate_correspondences,
+    build_correspondences,
+    TriangulationParameters,
+    rodrigues_and_translation_to_view_mat3x4
 )
 
 
@@ -36,12 +41,119 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         rgb_sequence[0].shape[0]
     )
 
-    # TODO: implement
-    frame_count = len(corner_storage)
-    view_mats = [pose_to_view_mat3x4(known_view_1[1])] * frame_count
-    corners_0 = corner_storage[0]
-    point_cloud_builder = PointCloudBuilder(corners_0.ids[:1],
-                                            np.zeros((1, 3)))
+    np.random.seed(42)
+    params = TriangulationParameters(5, 1, 0.2)
+    dist_coefs = np.array([])
+
+    frame_1 = known_view_1[0]
+    frame_2 = known_view_2[0]
+    processed_frames = [frame_1, frame_2]
+    frames_to_extract_pose = [i for i in range(len(corner_storage))
+                              if i != frame_1 and i != frame_2]
+
+    view_mats = [None for i in range(len(corner_storage))]
+    view_mats[frame_1] = pose_to_view_mat3x4(known_view_1[1])
+    view_mats[frame_2] = pose_to_view_mat3x4(known_view_2[1])
+
+    corrs = build_correspondences(corner_storage[frame_1],
+                                  corner_storage[frame_2])
+
+    points_3d, ids, _ = triangulate_correspondences(corrs,
+                                                    view_mats[frame_1],
+                                                    view_mats[frame_2],
+                                                    intrinsic_mat,
+                                                    params)
+
+    point_cloud_builder = PointCloudBuilder(ids=ids, points=points_3d)
+
+    selected_frame = None
+    ftep_copy = frames_to_extract_pose.copy()
+    while frames_to_extract_pose and ftep_copy:
+        if selected_frame is not None:
+            cos_score = []
+            for frame in processed_frames[:-1]:
+                cur_corrs = build_correspondences(corner_storage[frame],
+                                                  corner_storage[processed_frames[-1]])
+
+                points_3d, ids, med_cos = triangulate_correspondences(cur_corrs,
+                                                                      view_mats[frame],
+                                                                      view_mats[processed_frames[-1]],
+                                                                      intrinsic_mat,
+                                                                      params)
+
+                point_cloud_builder.add_points(ids, points_3d)
+                cos_score.append(med_cos)
+
+            # update points for top 10%
+            min_index = np.argsort(cos_score)[:int(np.ceil(len(cos_score) * 0.1))][::-1]
+
+            for index in min_index:
+                frame = processed_frames[:-1][index]
+
+                cur_corrs = build_correspondences(corner_storage[frame],
+                                                  corner_storage[processed_frames[-1]])
+
+                points_3d, ids, med_cos = triangulate_correspondences(cur_corrs,
+                                                                      view_mats[frame],
+                                                                      view_mats[processed_frames[-1]],
+                                                                      intrinsic_mat,
+                                                                      params)
+
+                point_cloud_builder.add_points(ids, points_3d)
+
+        print(f"triangulated points: {point_cloud_builder.points.shape[0]}")
+        x_dim = [point[0] for point in point_cloud_builder.points]
+        y_dim = [point[1] for point in point_cloud_builder.points]
+        z_dim = [point[2] for point in point_cloud_builder.points]
+        print(f"cloud point can be placed in parallelepiped with")
+        print(f"length: {max(x_dim) - min(x_dim)}")
+        print(f"width: {max(y_dim) - min(y_dim)}")
+        print(f"height: {max(z_dim) - min(z_dim)}")
+        print("")
+
+        succeeded = False
+        ftep_copy = frames_to_extract_pose.copy()
+        while not succeeded and ftep_copy:
+            selected_frame = np.random.choice(ftep_copy)
+            selected_points_ids = np.intersect1d(point_cloud_builder.ids,
+                                                 corner_storage[selected_frame].ids)
+
+            selected_points_2d = []
+            selected_points_3d = []
+
+            for corner_coords, corner_id in zip(corner_storage[selected_frame].points,
+                                                corner_storage[selected_frame].ids):
+                if corner_id in selected_points_ids:
+                    selected_points_2d.append(corner_coords)
+
+            for point_coords, point_id in zip(point_cloud_builder.points,
+                                              point_cloud_builder.ids):
+                if point_id in selected_points_ids:
+                    selected_points_3d.append(point_coords)
+
+            print(f"in process: {selected_frame} frame")
+            succeeded, rvec, tvec, inliers = cv2.solvePnPRansac(np.array(selected_points_3d),
+                                                                np.array(selected_points_2d),
+                                                                intrinsic_mat, dist_coefs,
+                                                                iterationsCount=100,
+                                                                reprojectionError=params.max_reprojection_error,
+                                                                confidence=0.99)
+
+            if succeeded:
+                print(f"{selected_frame} frame processing succeeded")
+                print(f"{inliers.shape[0]} inlier(s) used in {selected_frame} frame processing")
+                view_mats[selected_frame] = rodrigues_and_translation_to_view_mat3x4(rvec, tvec)
+                frames_to_extract_pose.remove(selected_frame)
+                processed_frames.append(selected_frame)
+            else:
+                print(f"{selected_frame} frame processing failed, selecting another frame...")
+                ftep_copy.remove(selected_frame)
+
+            print("")
+
+    for i in range(len(view_mats)):
+        if view_mats[i] is None:
+            view_mats[i] = view_mats[i - 1]
 
     calc_point_cloud_colors(
         point_cloud_builder,
@@ -49,8 +161,9 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
         view_mats,
         intrinsic_mat,
         corner_storage,
-        5.0
+        params.max_reprojection_error
     )
+
     point_cloud = point_cloud_builder.build_point_cloud()
     poses = list(map(view_mat3x4_to_pose, view_mats))
     return poses, point_cloud
